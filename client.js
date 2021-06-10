af5448218c77ca6894d2fd7005bf4ab69e597be6
Francisco Sousa <franciscoeds1987@hotmail.com>
2021-06-10 07:38:46 -0300

    Implementando uma requisição GET com JavaScript

--- client.js
+++ client.js
@@ -1,23 +1,28 @@
-function consultarRestaurantes() {
+function consultar() {
     $.ajax({
-        url: "http://api.algafood.local:8080/restaurantes",
-        type: "get",
-
-        success: function(response) {
-            $("#conteudo").text(JSON.stringify(response));
-        }
+      url: "http://api.algafood.local:8080/formas-pagamento",
+      type: "get",
+  
+      success: function(response) {
+        preencherTabela(response);
+      }
     });
-}
-
-function fecharRestaurante() {
-    $.ajax({
-        url: "http://api.algafood.local:8080/restaurantes/1/fechamento",
-        type: "put",
-
-        success: function(response) {
-            alert("Restaurante foi fechado!");
-        }
+  }
+  
+  function preencherTabela(formasPagamento) {
+    $("#tabela tbody tr").remove();
+  
+    $.each(formasPagamento, function(i, formaPagamento) {
+      var linha = $("<tr>");
+  
+      linha.append(
+        $("<td>").text(formaPagamento.id),
+        $("<td>").text(formaPagamento.descricao)
+      );
+  
+      linha.appendTo("#tabela");
     });
-}
-
-$("#botao").click(fecharRestaurante);
+  }
+  
+  
+  $("#btn-consultar").click(consultar);
